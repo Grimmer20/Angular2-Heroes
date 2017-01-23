5cda687cede920e5f3ffd67f6b0447c2b6e48419
Grimmer20 <m.m.grimm1@gmail.com>
2017-01-23 13:19:46 -0600

    Creating injection

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,15 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
 
 export class Hero {
   id: number;
   name: string;
 }
 
-// still getting mock data from AppComponent. Need to seperate and share //with components as needed.
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+heroes: Hero[];
+constructor(private heroService: HeroService) { }
 
 @Component({
   selector: 'my-app',
@@ -85,6 +75,8 @@ const HEROES: Hero[] = [
       border-radius: 4px 0 0 4px;
     }
   `]
+  providers: [HeroService]
+
 })
 export class AppComponent {
   title = 'Tour of Heroes';
