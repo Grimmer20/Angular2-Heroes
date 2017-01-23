5725896c96c43178c3c646bf3841768a4662e867
Grimmer20 <m.m.grimm1@gmail.com>
2017-01-23 12:57:46 -0600

    Seperate into single responsibility components.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+
 export class Hero {
   id: number;
   name: string;
 }
+
+// still getting mock data from AppComponent. Need to seperate and share //with components as needed.
+
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
@@ -15,11 +20,12 @@ const HEROES: Hero[] = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
 ];
+
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
+  <h1>{{title}}</h1>
+  <h2>My Heroes</h2>
     <ul class="heroes">
       <li *ngFor="let hero of heroes"
         [class.selected]="hero === selectedHero"
@@ -27,15 +33,9 @@ const HEROES: Hero[] = [
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
+
   styles: [`
     .selected {
       background-color: #CFD8DC !important;
@@ -90,6 +90,7 @@ export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
   selectedHero: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
