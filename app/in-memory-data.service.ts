5d51f336900f83ffa3fc7d0334a0cca2b00e92b0
Grimmer20 <m.m.grimm1@gmail.com>
2017-01-26 11:25:57 -0600

    Mom and Dad thanks.

--- app/in-memory-data.service.ts
+++ app/in-memory-data.service.ts
@@ -3,16 +3,16 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
    createDb() {
     let heroes = [
-      {id: 1, name: 'Aaron Rodgers'},
-      {id: 2, name: 'Brett Favre'},
+      {id: 1, name: 'Mom'},
+      {id: 2, name: 'Dad'},
       {id: 3, name: 'Bart Starr'},
       {id: 4, name: 'Reggie White'},
       {id: 5, name: 'LeRoy Butler'},
       {id: 6, name: 'Vince Lombardi'},
       {id: 7, name: 'Ray Nitsche'},
       {id: 8, name: 'Frozen Tundra'},
-      {id: 9, name: 'Mom'},
-      {id: 0, name: 'Dad'}
+      {id: 9, name: 'Aaron Rodgers'},
+      {id: 0, name: 'Brett Favre'}
     ];
     return {heroes};
   }
