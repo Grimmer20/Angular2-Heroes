f0470d45281206e5be2adedee038d2d0251448dc
Grimmer20 <m.m.grimm1@gmail.com>
2017-01-26 10:41:55 -0600

    Added CRUD functionality. Need to fix dashboard 404 error.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,14 @@
+// Imports core Angular components
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+// Imports for loading and configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+// Imports the custom components
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard.component';
 import { HeroDetailComponent }  from './hero-detail.component';
@@ -8,11 +16,14 @@ import { HeroesComponent }      from './heroes.component';
 import { HeroService }          from './hero.service';
 import { AppRoutingModule }     from './app-routing.module';
 
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   declarations: [
     AppComponent,
